50d85e042418ddc9e8b60aab01ff8b94076553ff
Marc Brinkmann <git@marcbrinkmann.de>
2016-05-08 17:39:28 +0200

    Automatic commmit using gittar.

---
+++ implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['libc'] = [];implementors['nix'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;<a class='enum' href='nix/enum.Errno.html' title='nix::Errno'>Errno</a>&gt; for <a class='struct' href='https://doc.rust-lang.org/nightly/std/io/error/struct.Error.html' title='std::io::error::Error'>Error</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;<a class='enum' href='nix/enum.Errno.html' title='nix::Errno'>Errno</a>&gt; for <a class='enum' href='nix/enum.Error.html' title='nix::Error'>Error</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;<a class='enum' href='nix/enum.Error.html' title='nix::Error'>Error</a>&gt; for <a class='struct' href='https://doc.rust-lang.org/nightly/std/io/error/struct.Error.html' title='std::io::error::Error'>Error</a>",];implementors['socketcan'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;<a class='enum' href='nix/enum.Error.html' title='nix::Error'>Error</a>&gt; for <a class='enum' href='socketcan/enum.CANSocketOpenError.html' title='socketcan::CANSocketOpenError'>CANSocketOpenError</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;<a class='struct' href='https://doc.rust-lang.org/nightly/std/io/error/struct.Error.html' title='std::io::error::Error'>Error</a>&gt; for <a class='enum' href='socketcan/enum.CANSocketOpenError.html' title='socketcan::CANSocketOpenError'>CANSocketOpenError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
