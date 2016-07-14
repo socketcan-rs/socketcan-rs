dfae5cdd13e5f72934f3e8a4bd7c35e055384620
Marc Brinkmann <git@marcbrinkmann.de>
2016-07-14 14:05:45 +0200

    Improved documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,36 @@
 //! (see https://www.kernel.org/doc/Documentation/networking/can.txt). This
 //! crate allows easy access to this functionality without having to wrestle
 //! libc calls.
+//!
+//! # An introduction to CAN
+//!
+//! The CAN bus was originally designed to allow microcontrollers inside a
+//! vehicle to communicate over a single shared bus. Messages called
+//! *frames* are multicast to all devices on the bus.
+//!
+//! Every frame consists of an ID and a payload of up to 8 bytes. If two
+//! devices attempt to send a frame at the same time, the device with the
+//! higher ID will notice the conflict, stop sending and reattempt to sent its
+//! frame in the next time slot. This means that the lower the ID, the higher
+//! the priority. Since most devices have a limited buffer for outgoing frames,
+//! a single device with a high priority (== low ID) can block communication
+//! on that bus by sending messages too fast.
+//!
+//! The Linux socketcan subsystem makes the CAN bus available as a regular
+//! networking device. Opening an network interface allows receiving all CAN
+//! messages received on it. A device CAN be opened multiple times, every
+//! client will receive all CAN frames simultaneously.
+//!
+//! Similarly, CAN frames can be sent to the bus by multiple client
+//! simultaneously as well.
+//!
+//! # Hardware and more information
+//!
+//! More information on CAN [can be found on Wikipedia](). When not running on
+//! an embedded platform with already integrated CAN components,
+//! [Thomas Fischl's USBtin](http://www.fischl.de/usbtin/) (see
+//! [section 2.4](http://www.fischl.de/usbtin/#socketcan)) is one of many ways
+//! to get started.
 
 extern crate hex;
 extern crate itertools;
