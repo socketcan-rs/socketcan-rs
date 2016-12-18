0a1997d6797a2ecd0d1e1ac9a61020de2cc6d10d
Marc Brinkmann <git@marcbrinkmann.de>
2016-12-18 13:08:59 +0100

    Added set_socket_option function.

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,45 @@
-use libc::timespec;
+use libc::{c_int, c_void, setsockopt, socklen_t, timespec};
+use std::io;
+use std::mem::size_of;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
+
+
+/// `setsockopt` wrapper
+///
+/// The libc `setsockopt` function is set to set various options on a socket.
+/// `set_socket_option` offers a somewhat type-safe wrapper that does not
+/// require messing around with `*const c_void`s.
+///
+/// A proper `std::io::Error` will be returned on failure.
+///
+/// Example use:
+///
+/// ```text
+/// let fd = ...;  // some file descriptor, this will be stdout
+/// set_socket_option(fd, SOL_TCP, TCP_NO_DELAY, 1 as c_int)
+/// ```
+///
+/// Note that the `val` parameter must be specified correctly; if an option
+/// expects an integer, it is advisable to pass in a `c_int`, not the default
+/// of `i32`.
+#[inline]
+pub fn set_socket_option<T>(fd: c_int, level: c_int, name: c_int, val: &T) -> io::Result<()> {
+    let rv = unsafe {
+        let val_ptr: *const T = val as *const T;
+
+        setsockopt(fd,
+                   level,
+                   name,
+                   val_ptr as *const c_void,
+                   size_of::<T>() as socklen_t)
+    };
+
+    if rv != 0 {
+        return Err(io::Error::last_os_error());
+    }
+
+    Ok(())
+}
 
 #[inline]
 pub fn duration_from_timeval(ts: timespec) -> Duration {
