aea834c7ca35000cb07b1d08c21113c3e2eda8eb
Marc Brinkmann <git@marcbrinkmann.de>
2016-12-18 12:52:17 +0100

    Move vcan-tests to submodule.

--- src/tests.rs
+++ src/tests.rs
@@ -1,29 +1,28 @@
 use ::CanSocket;
 
-#[cfg(feature = "vcan_tests")]
-use std::time;
-
-#[cfg(feature = "vcan_tests")]
-use ::ShouldRetry;
-
 #[test]
 fn test_nonexistant_device() {
     assert!(CanSocket::open("invalid").is_err());
 }
 
-#[test]
-#[cfg(feature = "vcan_tests")]
-fn vcan0_timeout() {
-    let cs = CanSocket::open("vcan0").unwrap();
-    cs.set_read_timeout(time::Duration::from_millis(100)).unwrap();
-    assert!(cs.read_frame().should_retry());
-}
 
-
-#[test]
 #[cfg(feature = "vcan_tests")]
-fn vcan0_set_error_mask() {
-    let cs = CanSocket::open("vcan0").unwrap();
-    cs.error_filter_drop_all().unwrap();
-    cs.error_filter_accept_all().unwrap();
+mod vcan_tests {
+    use ::CanSocket;
+    use std::time;
+    use ::ShouldRetry;
+
+    #[test]
+    fn vcan0_timeout() {
+        let cs = CanSocket::open("vcan0").unwrap();
+        cs.set_read_timeout(time::Duration::from_millis(100)).unwrap();
+        assert!(cs.read_frame().should_retry());
+    }
+
+    #[test]
+    fn vcan0_set_error_mask() {
+        let cs = CanSocket::open("vcan0").unwrap();
+        cs.error_filter_drop_all().unwrap();
+        cs.error_filter_accept_all().unwrap();
+    }
 }
