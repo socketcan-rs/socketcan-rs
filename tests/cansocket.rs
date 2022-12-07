db42de49dfa584e25acf004528b1141c9b181300
fpagliughi <fpagliughi@mindspring.com>
2022-12-07 04:09:18 -0500

    Hid the integration tests behind a 'vcan_tests' feature.

--- tests/cansocket.rs
+++ tests/cansocket.rs
@@ -1,8 +1,16 @@
+#[cfg(feature = "vcan_tests")]
 use socketcan::{
     frame::{IdFlags, ERR_MASK_ALL, ERR_MASK_NONE},
-    CanFrame, CanSocket, ShouldRetry, Socket,
+    CanFrame, ShouldRetry,
 };
+use socketcan::{CanSocket, Socket};
+
+#[cfg(feature = "vcan_tests")]
 use std::time;
+
+// The virtual CAN interface to use for tests.
+#[cfg(feature = "vcan_tests")]
+const VCAN: &str = "vcan0";
 
 #[test]
 fn test_nonexistant_device() {
@@ -10,59 +18,65 @@ fn test_nonexistant_device() {
 }
 
 #[test]
-fn vcan0_timeout() {
-    let cs = CanSocket::open("vcan0").unwrap();
+#[cfg(feature = "vcan_tests")]
+fn vcan_timeout() {
+    let sock = CanSocket::open(VCAN).unwrap();
     // Filter out _any_ traffic
-    cs.filter_drop_all().unwrap();
-    cs.set_read_timeout(time::Duration::from_millis(100))
+    sock.filter_drop_all().unwrap();
+    sock.set_read_timeout(time::Duration::from_millis(100))
         .unwrap();
 
-    assert!(cs.read_frame().should_retry());
-}
-
-#[test]
-fn vcan0_set_error_mask() {
-    let cs = CanSocket::open("vcan0").unwrap();
-    cs.set_error_mask(ERR_MASK_ALL).unwrap();
-    cs.set_error_mask(ERR_MASK_NONE).unwrap();
-}
-
-#[test]
-fn vcan0_enable_own_loopback() {
-    let cs = CanSocket::open("vcan0").unwrap();
-    cs.set_loopback(true).unwrap();
-    cs.set_recv_own_msgs(true).unwrap();
-
-    let frame = CanFrame::init(0x123, &[], IdFlags::RTR).unwrap();
-
-    cs.write_frame(&frame).unwrap();
-    cs.read_frame().unwrap();
-}
-
-// #[test]
-// fn vcan0_set_down() {
-//     let can_if = CanInterface::open("vcan0").unwrap();
-//     can_if.bring_down().unwrap();
-// }
-
-#[test]
-fn vcan0_test_nonblocking() {
-    let cs = CanSocket::open("vcan0").unwrap();
-    // Filter out _any_ traffic
-    cs.filter_drop_all().unwrap();
-    cs.set_nonblocking(true).unwrap();
-
-    // no timeout set, but should return immediately
-    assert!(cs.read_frame().should_retry());
+    assert!(sock.read_frame().should_retry());
 }
 
 #[test]
 #[cfg(feature = "vcan_tests")]
-fn vcan0_test_fd() {
-    let cs = CanFdSocket::open("vcan0").unwrap();
+fn vcan_set_error_mask() {
+    let sock = CanSocket::open(VCAN).unwrap();
+    sock.set_error_mask(ERR_MASK_ALL).unwrap();
+    sock.set_error_mask(ERR_MASK_NONE).unwrap();
+}
+
+#[test]
+#[cfg(feature = "vcan_tests")]
+fn vcan_enable_own_loopback() {
+    let sock = CanSocket::open(VCAN).unwrap();
+    sock.set_loopback(true).unwrap();
+    sock.set_recv_own_msgs(true).unwrap();
+
+    let frame = CanFrame::init(0x123, &[], IdFlags::RTR).unwrap();
+
+    sock.write_frame(&frame).unwrap();
+    sock.read_frame().unwrap();
+}
+
+// #[test]
+// fn vcan_set_down() {
+//     let can_if = CanInterface::open(VCAN).unwrap();
+//     can_if.bring_down().unwrap();
+// }
+
+#[test]
+#[cfg(feature = "vcan_tests")]
+fn vcan_test_nonblocking() {
+    let sock = CanSocket::open(VCAN).unwrap();
+    // Filter out _any_ traffic
+    sock.filter_drop_all().unwrap();
+    sock.set_nonblocking(true).unwrap();
+
+    // no timeout set, but should return immediately
+    assert!(sock.read_frame().should_retry());
+}
+
+/*
+#[test]
+#[cfg(feature = "vcan_tests")]
+fn vcan_test_fd() {
+    let sock = CanFdSocket::open(VCAN).unwrap();
     for _ in 0..3 {
-        let frame = cs.read_frame().unwrap();
+        let frame = sock.read_frame().unwrap();
         println!("Received frame: {:X}", frame);
-        cs.write_frame(&frame).unwrap();
+        sock.write_frame(&frame).unwrap();
     }
 }
+*/
